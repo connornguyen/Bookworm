ae5c1f7d3c45ccdd0b57c6fac6d8eac020223b34
Connor Nguyen <nguyendinhthanhcong2512@gmail.com>
2024-08-14 14:20:22 -0700

    Starting the project

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -5,32 +5,13 @@
 //  Created by Apple on 13/08/2024.
 //
 
+import SwiftData
 import SwiftUI
-
-struct PushButton: View {
-    let tittle: String
-    @Binding var showingButton: Bool
-    
-    var onColors = [Color.red, Color.yellow]
-    var offColors = [Color.black, Color.white]
-    
-    var body: some View {
-        Button(tittle) {
-            showingButton.toggle()
-        }
-        .padding()
-        .background(LinearGradient(colors: showingButton ? onColors : offColors, startPoint: .topLeading, endPoint: .bottomTrailing))
-        .clipShape(.capsule)
-        .foregroundStyle(.white)
-        
-    }
-}
-
 
 
 struct ContentView: View {
     
-    @State private var isOn = false
+    
     
     var body: some View {
         VStack {
